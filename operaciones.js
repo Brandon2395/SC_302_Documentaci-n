4db3777592bcab323f4b55f2e9bcccfc3e6902dd
RyaVal <124115614+RyaVal@users.noreply.github.com>
2023-02-03 23:24:25 -0600

    Metodo factorial a operaciones

--- operaciones.js
+++ operaciones.js
@@ -35,3 +35,12 @@ function promedio(numeros) {
     } 
     return suma / numeros.length; 
 } 
+
+// Desarrollador 4
+function factorial(n) {
+     let resultado = 1;
+     for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
